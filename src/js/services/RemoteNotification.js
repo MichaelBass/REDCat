20f9c95b70a91198679bd9af29d4034344f0b727
Michael Bass <michael-bass@northwestern.edu>
2016-10-03 17:11:55 -0500

    add updateReminder

--- src/js/services/RemoteNotification.js
+++ src/js/services/RemoteNotification.js
@@ -1,30 +1,27 @@
-(function() {
+ (function() {
   'use strict';
-
-  // Wraps Cordova device plugin
-  function RemoteNotification($http, device) {
-
+  
+  function RemoteNotification($http,$window) {
+  
+    var mobilePlatform = $window.device.platform;
+    
     this.updateReminder = function updateReminder( token, studyName, reminder ) {
-
-      var mobilePlatform = 'iOS';
-      if (device.platform == 'android' || device.platform == 'Android' || device.platform == 'amazon-fireos' ) {
-          mobilePlatform = 'android';
-      }
-      var api = JSON.parse(localStorage['settings'])[0];
-      return $http({
-        method:'POST',
-        url: api.notificationserver + 'main?registerToken=true&mobilePlatform='+ mobilePlatform + '&token='+ token + '&protocol=' + studyName + '&reminder=' + reminder, 
-        data: ''
-        })
+      
+    var api = JSON.parse(localStorage['settings'])[0];
+    return $http({
+       method:'POST',
+       url: api.notificationserver + 'main?registerToken=true&mobilePlatform='+ mobilePlatform + '&token='+ token + '&protocol=' + studyName + '&reminder=' + reminder,
+       data: ''
+       })
     }
-
+  
   }
-
-  function RemoteNotificationFactory($http, device) {
-    return new RemoteNotification($http, device);
+  
+  function RemoteNotificationFactory($http,$window) {
+    return new RemoteNotification($http,$window);
   }
-
+  
   angular.module('redcat.services')
-     .factory('remoteNotification',['$http','device',RemoteNotificationFactory]);
-
-})();
+  .factory('remoteNotification',['$http','$window',RemoteNotificationFactory]);
+  
+  })();
