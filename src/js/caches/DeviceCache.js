84e385a736903aa255c306beaca8138e34a8b7bc
Michael Bass <michael-bass@northwestern.edu>
2016-10-03 17:04:07 -0500

    add delegate

--- src/js/caches/DeviceCache.js
+++ src/js/caches/DeviceCache.js
@@ -5,8 +5,10 @@
     this.KEY = 'devices';
 
     cache.delegate(this, 'persist');
+    cache.delegate(this, 'persistItem');
     cache.delegate(this, 'fetchAllDirty');
     cache.delegate(this, 'markClean');
+    cache.delegate(this, 'destroyAll');
   }
 
   function DeviceCacheFactory(resourceCache) {
